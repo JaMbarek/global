4d0de670eca08fc60b1e53e5cfccaf7862790088
liuxianglei@m-chain.com <liuxianglei@m-chain.com>
2018-12-18 19:18:47 +0800

    添加携程apollo配置支持

--- basic/gateway/src/main/java/com/cloud/mvc/example/basic/gateway/security/AccessDeniedHandler.java
+++ basic/gateway/src/main/java/com/cloud/mvc/example/basic/gateway/security/AccessDeniedHandler.java
@@ -1,31 +1,31 @@
-package com.cloud.mvc.example.basic.gateway.security;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-/**
- * 授权拒绝处理器，覆盖默认的OAuth2AccessDeniedHandler 包装失败信息到DeniedException
- * 
- */
-@Slf4j
-@Component
-public class AccessDeniedHandler extends OAuth2AccessDeniedHandler {
-
-	@Override
-	public void handle(HttpServletRequest request, HttpServletResponse response,
-                       AccessDeniedException authException) throws IOException, ServletException {
-		log.info("授权失败，禁止访问 {}", request.getRequestURI());
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("UTF-8");
-		PrintWriter printWriter = response.getWriter();
-		printWriter.append("授权失败，禁止访问");
-	}
-}
+//package com.cloud.mvc.example.basic.gateway.security;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.access.AccessDeniedException;
+//import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+///**
+// * 授权拒绝处理器，覆盖默认的OAuth2AccessDeniedHandler 包装失败信息到DeniedException
+// *
+// */
+//@Slf4j
+//@Component
+//public class AccessDeniedHandler extends OAuth2AccessDeniedHandler {
+//
+//	@Override
+//	public void handle(HttpServletRequest request, HttpServletResponse response,
+//                       AccessDeniedException authException) throws IOException, ServletException {
+//		log.info("授权失败，禁止访问 {}", request.getRequestURI());
+//		response.setCharacterEncoding("UTF-8");
+//		response.setContentType("UTF-8");
+//		PrintWriter printWriter = response.getWriter();
+//		printWriter.append("授权失败，禁止访问");
+//	}
+//}
--- business/system/src/main/java/com/cloud/mvc/example/business/system/SystemApplication.java
+++ business/system/src/main/java/com/cloud/mvc/example/business/system/SystemApplication.java
@@ -1,5 +1,6 @@
 package com.cloud.mvc.example.business.system;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +10,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication(scanBasePackages = "com.cloud.mvc.example.business.**")
 @EnableDiscoveryClient
 @MapperScan("com.cloud.mvc.example.business.system.dao")
+@EnableApolloConfig
 public class SystemApplication {
 
     public static void main(String[] args) {
-
+        System.setProperty("env", "dev");
         new SpringApplicationBuilder(SystemApplication.class)
                 .web(WebApplicationType.SERVLET)
                 .run(args);
