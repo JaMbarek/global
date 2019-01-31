e036401a808bf5168ba51ca6d8ad12f313723837
liuxianglei@m-chain.com <liuxianglei@m-chain.com>
2019-01-31 18:37:48 +0800

    添加databus模块

---
+++ databus/relay/src/main/java/com/example/databus/client/Main.java
@@ -0,0 +1,49 @@
+package com.example.databus.client;
+
+import com.linkedin.databus.container.netty.HttpRelay;
+import com.linkedin.databus.core.util.InvalidConfigException;
+import com.linkedin.databus2.core.DatabusException;
+import com.linkedin.databus2.relay.DatabusRelayMain;
+import com.linkedin.databus2.relay.config.PhysicalSourceStaticConfig;
+
+import java.io.IOException;
+
+public class Main extends DatabusRelayMain {
+
+    public Main() throws IOException, InvalidConfigException, DatabusException {
+        this(new HttpRelay.Config(), null);
+    }
+
+    public Main(HttpRelay.Config config, PhysicalSourceStaticConfig[] pConfigs)
+            throws IOException, InvalidConfigException, DatabusException {
+        this(config.build(), pConfigs);
+    }
+
+    public Main(HttpRelay.StaticConfig config, PhysicalSourceStaticConfig[] pConfigs)
+            throws IOException, InvalidConfigException, DatabusException {
+        super(config, pConfigs);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+//E:\github\global\databus\relay\src\main\resources\source\person.json
+
+        HttpRelay.Cli cli = new HttpRelay.Cli();
+        cli.setDefaultPhysicalSrcConfigFiles("E:\\github\\global\\databus\\relay\\src\\main\\resources\\source\\person.json");
+
+        cli.processCommandLineArgs(args);
+        cli.parseRelayConfig();
+
+        PhysicalSourceStaticConfig[] pStaticConfigs = cli.getPhysicalSourceStaticConfigs();
+
+        HttpRelay.StaticConfig staticConfig = cli.getRelayConfigBuilder().build();
+
+        // Create and initialize the server instance
+        DatabusRelayMain serverContainer = new DatabusRelayMain(staticConfig, pStaticConfigs);
+
+        serverContainer.initProducers();
+        serverContainer.registerShutdownHook();
+        serverContainer.startAndBlock();
+    }
+
+}
---
+++ databus/scripts/install-jar.cmd
@@ -0,0 +1,7 @@
+mvn install:install-file -Dfile=E:\github\global\databus\lib\ojdbc6-11.2.0.2.0.jar -DgroupId=com.oracle.ojdbc6 -DartifactId=ojdbc6 -Dversion=11.2.0.2.0 -Dpackaging=jar
+
+mvn install:install-file -Dfile=E:\github\global\databus\lib\open-replicator-1.0.7.jar -DgroupId=com.google -DartifactId=open-replicator -Dversion=1.0.7 -Dpackaging=jar
+
+mvn install:install-file -Dfile=E:\github\global\databus\lib\gentlyweb-utils-1.5.jar -DgroupId=net.sf.josql -DartifactId=gentlyweb-utils -Dversion=1.5 -Dpackaging=jar
+
+mvn install:install-file -Dfile=E:\github\global\databus\lib\josql-1.5.jar -DgroupId=net.sf.josql -DartifactId=josql -Dversion=1.0.5 -Dpackaging=jar
