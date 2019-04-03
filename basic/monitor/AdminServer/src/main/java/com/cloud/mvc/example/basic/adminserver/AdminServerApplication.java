745a6466984f5844d141045b6d8cf6823a7754ef
doublecancel <471567694@qq.com>
2019-04-03 14:41:19 +0800

    修改监控配置

--- basic/monitor/AdminServer/src/main/java/com/cloud/mvc/example/basic/adminserver/AdminServerApplication.java
+++ basic/monitor/AdminServer/src/main/java/com/cloud/mvc/example/basic/adminserver/AdminServerApplication.java
@@ -4,10 +4,14 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
+@EnableTurbine
+@EnableHystrixDashboard
 public class AdminServerApplication {
 
     public static void main(String[] args) {
